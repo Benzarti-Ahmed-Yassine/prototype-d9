fecd76682dc9687e762078c56a7a13e5d74522a9
Benzarti <121379135+Benzarti-Ahmed-Yassine@users.noreply.github.com>
2025-08-06 10:16:33 +0100

    Updated postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
-import tailwindcss from '@tailwindcss/postcss';
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
-    tailwindcss,
+    '@tailwindcss/postcss',
   ],
 };
 
